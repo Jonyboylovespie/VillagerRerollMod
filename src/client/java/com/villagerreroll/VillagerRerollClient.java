be7025ed11f9727e9f02daea1afde61e311b8c9b
Jonyboylovespie <jonathan.frawley@outlook.com>
2026-05-19 15:53:28 -0400

    Update VillagerRerollClient.java

--- src/client/java/com/villagerreroll/VillagerRerollClient.java
+++ src/client/java/com/villagerreroll/VillagerRerollClient.java
@@ -1,7 +1,6 @@
 package com.villagerreroll;
 
 import java.nio.file.Path;
-import java.util.Arrays;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import com.villagerreroll.autolibrarian.AutoLibrarianController;
@@ -9,10 +8,10 @@ import com.villagerreroll.screen.TargetBookListScreen;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.Options;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
@@ -30,7 +29,6 @@ public final class VillagerRerollClient implements ClientModInitializer {
 	private static final KeyMapping OPEN_MENU_KEY = new KeyMapping(
 		"key.villager_reroll.open_menu", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_R,
 		KeyMapping.Category.MISC);
-	private static boolean keyBindingRegistered;
 
 	public static AutoLibrarianController getController() {
 		return CONTROLLER;
@@ -38,13 +36,12 @@ public final class VillagerRerollClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		ensureKeyBindingRegistered(Minecraft.getInstance());
+		KeyMappingHelper.registerKeyMapping(OPEN_MENU_KEY);
 		ClientTickEvents.END_CLIENT_TICK.register(VillagerRerollClient::onClientTick);
 		UseBlockCallback.EVENT.register(VillagerRerollClient::onUseBlock);
 	}
 
 	private static void onClientTick(Minecraft client) {
-		ensureKeyBindingRegistered(client);
 		while(OPEN_MENU_KEY.consumeClick()) {
 			Screen current = client.screen;
 			if(current != null && current.getClass().getPackageName().equals("com.villagerreroll.screen"))
@@ -67,31 +64,5 @@ public final class VillagerRerollClient implements ClientModInitializer {
 			return InteractionResult.SUCCESS;
 
 		return InteractionResult.PASS;
-	}
-
-	private static void ensureKeyBindingRegistered(Minecraft client) {
-		if(keyBindingRegistered || client == null || client.options == null)
-			return;
-
-		Options options = client.options;
-		KeyMapping[] current = options.keyMappings;
-		for(KeyMapping mapping : current) {
-			if(mapping.getName().equals(OPEN_MENU_KEY.getName())) {
-				keyBindingRegistered = true;
-				return;
-			}
-		}
-
-		KeyMapping[] updated = Arrays.copyOf(current, current.length + 1);
-		updated[current.length] = OPEN_MENU_KEY;
-
-		try {
-			var field = Options.class.getField("keyMappings");
-			field.setAccessible(true);
-			field.set(options, updated);
-			keyBindingRegistered = true;
-		}catch(ReflectiveOperationException e) {
-			throw new IllegalStateException("Failed to register key binding", e);
-		}
 	}
 }
